b7cdd3c2dc7389a68c8deefff2d14e08a8c7a4a1
unknown <bmason06@CSPC101.dcs.bbk.ac.uk>
2014-11-29 15:58:58 +0000

    Day 10 exercises

---
+++ Day_10/StringExtended.java
@@ -0,0 +1,11 @@
+/**
+ * A class that attempts to extent final class String
+ * 1 error: cannot inherit from final String
+ */
+public class StringExtended extends String {
+
+    public void printEven() {
+        System.out.println("Prints even characters only.");
+    }
+
+}
